a1af0fb4838c1f9759e7bd4bce1629dc9a57e2fa
Ubuntu <201201886@csecnu.v52bed5ic50ejeplw3mbfofysf.mx.internal.cloudapp.net>
2016-03-13 16:11:57 +0000

    comment

--- week2/q4.R
+++ week2/q4.R
@@ -1,9 +1,15 @@
+# ggplot2 라이브러리와 MASS 임포트
 library(ggplot2)
 library(MASS)
+# Cars93 데이터를 가져온다.
 data("Cars93")
 cars93 <- Cars93
+# Cars93$Type을 플롯팅하게 지정한다.
 g <- ggplot(cars93, aes(x = Type))
-b <- geom_bar(fill='white', color='steelblue') 
+# Bar 그래프의 색깔을 지정한다.
+b <- geom_bar(fill='white', color='steelblue')
+# 그래프의 타이틀을 지정한다. 
 t <- ggtitle("Bar Chart of Frequency by Car Type")
+# 해당 설정을 모두 합쳐서 실행한다.
 g + b + t
 
--- week2/sum.py
+++ week2/sum.py
@@ -1,11 +1,18 @@
+#-*- coding: utf-8 -*-
+# 필요한 라이브러리를 임포트
 import urllib2
 import json
 
 serviceurl = 'http://python-data.dr-chuck.net/comments_42.json'
+# urllib2로 http response 결과를 가져온다.
 response = urllib2.urlopen(serviceurl)
+# 가져온 response 결과를 json라이브러리가 읽을수 있게 해준다.
 data = json.load(response)
 sum = 0
+# json 구조 comments 하부에 names와 count가 배열로 묶여있음
+# 따라서 data['comments'] 아래의 count 요소를 전부다 더하면 된다.
 for i in data['comments']:
     sum += i['count']
 
+# 결과를 출력
 print sum
