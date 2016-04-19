8fc0d192653031f12c4a2c9786ee717b1f7e6b1a
Dong-hee Na <corona10@gmail.com>
2016-04-19 17:46:52 +0900

    fix

--- week5/hw2.R
+++ week5/hw2.R
@@ -1,6 +1,5 @@
 library(ggmap)
 setwd('/home/parallels/cnu_datascience/week5')
-# 아래 둘 중 되는 것으로 한다. 유닉스/ 윈도우의 차이. 
 loc <- read.csv("toy_lib.csv",header=T)
 head(loc)
 kor <- get_map("seoul", zoom=11, maptype = "roadmap") # 지도를 가져온다.
