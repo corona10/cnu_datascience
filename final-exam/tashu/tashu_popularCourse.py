1905458f3af70cfabae30caf1a77241ccc4d0fd3
Dong-hee, Na <corona10@gmail.com>
2016-06-12 06:43:37 +0000

    final

--- final-exam/tashu/tashu_popularCourse.py
+++ final-exam/tashu/tashu_popularCourse.py
@@ -2,7 +2,7 @@
 from operator import add
 from pyspark import SparkConf, SparkContext
 
-conf = SparkConf().setAppName("tashu-stations")
+conf = SparkConf().setAppName("tashu-top10Course")
 sc = SparkContext(conf = conf)
 
 def loadStationInfo():
@@ -10,38 +10,36 @@ def loadStationInfo():
     with open("station.txt") as f:
         for line in f:
             fields = line.split(',')
-            stationLocations[int(fields[0])] = fields[2]
+            stationLocations[int(fields[0])] = fields[3]
     return stationLocations
 
 def parseLine(line):
     fields = line.split(",")
-    station_no = fields[1]
-    return station_no
+    start_no = fields[1]
+    dest_no = fields[3]
+    return (start_no, dest_no)
 
 def castToInteger(x):
     try:
-        return (int(x.split(',')[1]), 1)
+        return ((int(x.split(',')[1]), int(x.split(',')[3])), 1)
     except ValueError:
-        return (-1, 1)
+        return ((-1, -1), 1)
 
 station_names = sc.broadcast(loadStationInfo())
-def changeToArea((id, count)):
+def changeToArea(((start, dst), count)):
     try:
-        return (station_names.value[id], count)
+        return ((station_names.value[start], station_names.value[dst]), count)
     except KeyError:
         return ('기타', count)
-#station_names = sc.broadcast(loadStationInfo())
+
 lines = sc.textFile("tashu_rental_log.txt")
 stations = lines.map(castToInteger)
 stations = stations.partitionBy(4)
 stationCount = stations.reduceByKey(lambda x,y: x+y)
 stationCount = stationCount.map(changeToArea)
-stationCount = stationCount.reduceByKey(lambda x,y: x+y)
-#stationCount = stationCount.sortByKey()
+
 result = stationCount.collect()
+result = stationCount.takeOrdered(10, key = lambda x: -x[1])
 for r in result:
-    print r[0],':',r[1]
-#for r in result:
-#    print r
-#for station_no, count in stationCount.items():
-#    print station_no, count
+    station = r[0]
+    print station[0],'~',station[1],':',r[1]
